77000f6218d3288cebb594761d8d07bfc300a0e3
NAUFAL NABBIH AL SHIDDIQ <naufal.nabbih.20@student.se.ittelkom-sby.ac.id>
2024-01-10 21:43:07 +0700

    laporan absensi santri

--- app/laporan/page.js
+++ app/laporan/page.js
@@ -1,6 +1,32 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import LaporanKehadiran from "../../components/LaporanKehadiran.js";
+
 export default function Laporan() {
-    return (
-      <h1 className=''>Laporan</h1>
-    )
-  }
-  
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:8000/api/santri/report")
+      .then((response) => response.json())
+      .then((json) => setUsers(json.data))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <h1 className="text-center">Absensi Santri</h1>
+          {users.length > 0 && (
+            <LaporanKehadiran data={users} />
+          )}
+        </>
+      )}
+    </div>
+  );
+}
