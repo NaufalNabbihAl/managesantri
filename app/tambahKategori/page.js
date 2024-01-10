3f12b921536bfe8b94902cd8f3d1faa31175ba39
NAUFAL NABBIH AL SHIDDIQ <naufal.nabbih.20@student.se.ittelkom-sby.ac.id>
2024-01-10 21:42:49 +0700

    tambah kategori baru

---
+++ app/tambahKategori/page.js
@@ -0,0 +1,11 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import TambahKategori from "../../components/TambahKategori.js";
+
+export default function tambahSantri() {
+    return(
+        
+        <TambahKategori />
+    )
+}
