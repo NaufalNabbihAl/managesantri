b4003fa835e6438e05ac046e385f1bb76a09040a
NAUFAL NABBIH AL SHIDDIQ <naufal.nabbih.20@student.se.ittelkom-sby.ac.id>
2024-01-10 21:42:16 +0700

    tambah santri

---
+++ app/tambahSantri/page.js
@@ -0,0 +1,10 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import TambahSantri from "../../components/TambahSantri.js";
+
+export default function tambahSantri() {
+    return(
+        <TambahSantri />
+    )
+}
